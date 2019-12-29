d3e5a6e5cfd47de4602298b1e98fcae8ccc71c1e
Zarif <uzbzarif@gmail.com>
2019-12-29 16:50:06 +0900

    Lesson-5

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Text("ZARIF"),
-));
-
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('New Title'),
+            centerTitle: true,
+          ),
+          body: Center(
+            child: Text('Body'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Text('Click'),
+            onPressed: () {},
+          ),
+        ),
+      ),
+    );
